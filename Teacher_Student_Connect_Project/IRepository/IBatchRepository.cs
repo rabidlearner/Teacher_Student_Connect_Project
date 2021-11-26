65f282544286d6b2462e81abc116eee0c3faced7
VaishnaviAchuri <94669943+VaishnaviAchuri@users.noreply.github.com>
2021-11-26 23:39:26 +0530

    Add files via upload

---
+++ Teacher_Student_Connect_Project/IRepository/IBatchRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Teacher_Student_Connect_Project.Models;
+
+namespace Teacher_Student_Connect_Project.IRepository
+{
+    public interface IBatchRepository
+    {
+        List<Batch> GetBatches();
+        Batch GetBatches(int id);
+        void PostBatches(Batch subject);
+    }
+}
---
+++ Teacher_Student_Connect_Project/IRepository/ISubjectRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Teacher_Student_Connect_Project.Models;
+
+namespace Teacher_Student_Connect_Project.IRepository
+{
+    public interface ISubjectRepository
+    {
+        List<Subject> GetSubjects();
+        Subject GetSubjects(int id);
+        void PostSubjects(Subject subject); 
+    }
+}
