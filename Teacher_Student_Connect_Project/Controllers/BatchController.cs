cef2d6eddb47c6e23138b72c02c02a72b627357e
VaishnaviAchuri <94669943+VaishnaviAchuri@users.noreply.github.com>
2021-11-27 16:00:13 +0530

    Add files via upload

---
+++ Teacher_Student_Connect_Project/Controllers/BatchController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Teacher_Student_Connect_Project.IRepository;
+using Teacher_Student_Connect_Project.Models;
+using Teacher_Student_Connect_Project.Repository;
+
+namespace Teacher_Student_Connect_Project.Controllers
+{
+    public class BatchController:Controller
+    {
+        private readonly IBatchRepository batchRepository;
+
+        public BatchController(IBatchRepository batchRepository)
+        {
+            this.batchRepository = batchRepository;
+        }
+        public IActionResult Index()
+        {
+            List<Batch> batches = new List<Batch>();
+            batches = batchRepository.GetBatches();
+            return View(batches);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Batch batch)
+        {
+            if (ModelState.IsValid)
+            {
+                batchRepository.PostBatches(batch);
+                return RedirectToAction("Index");
+            }
+            return View(batch);
+        }
+    }
+}
---
+++ Teacher_Student_Connect_Project/Controllers/SubjectController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Teacher_Student_Connect_Project.IRepository;
+using Teacher_Student_Connect_Project.Models;
+using Teacher_Student_Connect_Project.Repository;
+
+namespace Teacher_Student_Connect_Project.Controllers
+{
+    public class SubjectController:Controller
+    {
+        private readonly ISubjectRepository subjectRepository;
+
+        public SubjectController(ISubjectRepository subjectRepository)
+        {
+            this.subjectRepository = subjectRepository;
+        }
+        public IActionResult Index()
+        {
+            List<Subject> subjects = new List<Subject>();
+            subjects = subjectRepository.GetSubjects();
+            return View(subjects);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Subject subject)
+        {
+            if (ModelState.IsValid)
+            {
+                subjectRepository.PostSubjects(subject);
+                return RedirectToAction("Index");
+            }
+            return View(subject);
+        }
+    }
+}
