e33385ae00bbc8e8acd8eeed8d3768d8d171311c
VaishnaviAchuri <94669943+VaishnaviAchuri@users.noreply.github.com>
2021-12-02 22:36:51 +0530

    Add files via upload

---
+++ Teacher_Student_Connect_Project/ViewModels/AdminDataViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Teacher_Student_Connect_Project.ViewModels
+{
+    public class AdminDataViewModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Dob { get; set; }
+        public string Gender { get; set; }
+        public string PhoneNumber { get; set; }
+        public int UserId { get; set; }
+        public string Website { get; set; }
+    }
+}
---
+++ Teacher_Student_Connect_Project/ViewModels/AdminViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Teacher_Student_Connect_Project.Models;
+
+namespace Teacher_Student_Connect_Project.ViewModels
+{
+    public class AdminViewModel
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int DocumnetId { get; set; }
+        //public Document Document { get; set; }
+        public List<Document> DocumentList { get; set; }
+    }
+}
